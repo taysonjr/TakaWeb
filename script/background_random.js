0d8a4eea26c6ef01a897d709af5f01ec3f495220
Taka <63388371+taysonjr@users.noreply.github.com>
2021-01-23 23:38:42 +0900

    Update background_random.js

--- script/background_random.js
+++ script/background_random.js
@@ -6,8 +6,23 @@
     images: {
       dir: 'img/index/',
       path: [
+        '001.jpg',
+        '002.jpg',
         '003.jpg',
-        '002.jpg'
+        '004.jpg',
+        '005.jpg',
+        '006.jpg',
+        '007.jpg',
+        '008.jpg',
+        '009.jpg',
+        '010.jpg',
+        '011.jpg',
+        '012.jpg',
+        '013.jpg',
+        '014.jpg',
+        '015.jpg',
+        '016.jpg',
+        '017.jpg'
       ],
       current: 0,
       duration: 2000
